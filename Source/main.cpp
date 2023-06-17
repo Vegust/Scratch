6b728c34eee308f1d50b4a6abbf461e9d4205e90
vegust <vegust@live.com>
2023-06-18 00:14:15 +0200

    glfw example

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,4 +2,48 @@
 // Created by Vegust on 17.06.2023.
 //
 
-#include "Platform/Win32/win32_window.cpp"
+//#include "Platform/Win32/win32_window.cpp"
+
+#include "Utils/glfw.h"
+
+int main()
+{
+	GLFWwindow* window;
+	
+	/* Initialize the library */
+	if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f,-0.5f);
+		glVertex2f(-0.0f,0.5f);
+		glVertex2f(0.5f,-0.5f);
+		glEnd();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
