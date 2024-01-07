ff70261a6430e250c352e9a15e93a244ff84dad7
vegust <vegust@live.com>
2024-01-07 20:36:23 +0100

    formatting interface

--- Source/Core/String/str_format.h
+++ Source/Core/String/str_format.h
@@ -5,6 +5,56 @@
 
 namespace strings {
 
+struct format_argument {
+	enum class type { string_arg, uint_arg, int_arg, float_arg, double_arg, pointer_arg } Type;
 
+	union {
+		str_view String;
+		u64 UInt;
+		s64 Int;
+		float Float;
+		double Double;
+		void* Pointer;
+	};
 
+	FORCEINLINE constexpr format_argument(str_view Val) : String{Val} {
+	}
+
+	FORCEINLINE constexpr format_argument(u64 Val) : UInt{Val} {
+	}
+
+	FORCEINLINE constexpr format_argument(s64 Val) : Int{Val} {
+	}
+
+	FORCEINLINE constexpr format_argument(float Val) : Float{Val} {
+	}
+
+	FORCEINLINE constexpr format_argument(double Val) : Double{Val} {
+	}
+
+	FORCEINLINE constexpr format_argument(void* Val) : Pointer{Val} {
+	}
+};
+
+template <u64 NumArguments>
+struct format_argument_array {
+	format_argument Data[NumArguments];
+
+	template <typename... argument_types>
+	format_argument_array(const argument_types&... Arguments) : Data{Arguments...} {
+	}
+};
+
+template <typename... argument_types>
+str Format(str_view String, const argument_types&... Arguments) {
+	constexpr u64 NumArguments = sizeof...(Arguments);
+	format_argument_array<NumArguments> ArgArray{Arguments...};
+	return Format(String, ArgArray);
 }
+
+template <u64 NumArguments>
+str Format(str_view String, const format_argument_array<NumArguments>& Arguments) {
+	return str{String};
+}
+
+}	 // namespace strings
--- Source/Core/core.h
+++ Source/Core/core.h
@@ -4,6 +4,7 @@
 #include "Containers/dyn_array.h"
 #include "String/str.h"
 #include "String/str_conversions.h"
+#include "String/str_format.h"
 #include "Utility/hash.h"
 #include "Templates/concepts.h"
 #include "Templates/result.h"
